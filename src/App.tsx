0c582493427da6b646f56450b7b80f3795df0870
Thiago Cedran Santos <thiagocdn@TCedran-Dell>
2022-10-11 20:13:18 -0300

    added first tokens

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export function App() {
   return (
     <>
       <h1 className='font-bold text-5xl text-violet-500'>Hello World!</h1>
-      <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>Enviar</button>
+      <button className='bg-cyan-300 font-medium px-4 py-2 rounded text-white hover:bg-cyan-500'>Enviar</button>
     </>
   )
 }

8db4188e5b1024b1d79ff16059d011628d0ee031
Thiago Cedran Santos <thiagocdn@TCedran-Dell>
2022-10-12 22:14:37 -0300

    Added button on storybook

---
+++ src/components/text-input.tsx
@@ -0,0 +1,14 @@
+import { clsx } from 'clsx'
+
+export interface TextInputProps {
+}
+
+export function TextInput({}: TextInputProps) {
+
+    return(
+        <input className={
+            clsx('',
+            )}
+        />
+    )
+}

42a9d11d1add0201dd85f30821c89197b5f7586a
Thiago Cedran Santos <thiagocdn@TCedran-Dell>
2022-10-13 10:04:29 -0300

    Finished class

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,66 @@
+import { Envelope, Lock } from 'phosphor-react'
+import { Button } from './components/button'
+import { Checkbox } from './components/checkbox'
+import { Heading } from './components/heading'
+import { Text } from './components/text'
+import { TextInput } from './components/text-input'
+import ReactLogo from './logo'
 import './styles/global.css'
 
 export function App() {
   return (
-    <>
-      <h1 className='font-bold text-2xl text-violet-500'>Hello World!</h1>
-      <button className='bg-cyan-300 font-medium px-4 py-2 rounded text-white hover:bg-cyan-500'>Enviar</button>
-    </>
+    <div className='w-screen h-screen bg-gray-900 flex flex-col items-center justify-center'>
+      <header className='flex flex-col items-center'>
+        <ReactLogo />
+
+        <Heading size='lg' className='mt-4'>
+          Ignite Lab
+        </Heading>
+
+        <Text size="lg" className='text-gray-400 mt-1'>
+          Faça login e comece a usar!
+        </Text>
+      </header>
+
+      <form className='flex flex-col items-stretch w-full max-w-sm mt-10 gap-4' action="submit">
+        <label htmlFor="email" className='flex flex-col gap-2'>
+          <Text className='font-semibold'>Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input type='email' id='email' placeholder='Digite seu e-mail' />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor='password' className='flex flex-col gap-2'>
+          <Text className='font-semibold'>Senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input type='password' id='password' placeholder='********' />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor='remember' className='flex items-center gap-2'>
+          <Checkbox id='remember' />
+          <Text size='sm' className='text-gray-200'>Lembrar de mim por 30 dias</Text>
+        </label>
+
+        <Button type='submit' className='mt-4'>
+          Entrar na Plataforma
+        </Button>
+      </form>
+
+      <footer className='flex flex-col items-center gap-4 mt-8'>
+        <Text size='sm'>
+          <a href="" className='text-gray-400 underline hover:text-gray-200'>Esqueceu sua senha?</a>
+        </Text>
+        <Text size='sm'>
+          <a href="" className='text-gray-400 underline hover:text-gray-200'>Não possui conta? Crie agora!</a>
+        </Text>
+      </footer>
+    </div>
   )
 }
